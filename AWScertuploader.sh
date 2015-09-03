5f8f3f826d5893e37e498d4d46485c90ad3c92de
Matt Burdan <matt.burdan@lookout.com>
2015-09-03 12:37:14 -0700

    Adding the aws certificate upload script

---
+++ AWScertuploader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+script_dir=$(PWD)
+
+printf "AWS Profile?: "
+read awsprofile
+printf "Certificate name?: "
+read certname
+printf "Intermediate/Certificate chain name?: "
+read chain
+printf "Is the path $script_dir/ ?(y or n): "
+read paths
+
+if [ "$paths" = "y" ]
+then
+exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$script_dir/$chain.crt #This assumes chain lives in the same file as this script
+else
+printf "What is the path the certificate is in?: "
+read newpaths
+exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$newpaths/$chain.crt #This assumes chain lives in the same file as this script
+fi
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# AWS-Certificate-Uploader
+# aws_certificate_uploader
 A script to upload server certificates without typing in the command every time.
