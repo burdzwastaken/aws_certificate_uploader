57d11b5f2a10c840b4dd6e530618c567b24dd72f
Matt Burdan <matt.burdan@lookout.com>
2015-09-03 15:06:24 -0700

    Adding change for different chain path

--- aws_cert_uploader.sh
+++ aws_cert_uploader.sh
@@ -9,28 +9,53 @@ printf "Certificate name?: "
 read certname
 printf "Intermediate/Certificate chain name?: "
 read chain
+printf "Is this the Intermediate/Certificate chain directory? - $chain_dir/ (y or enter new path): "
+read newchain_dir
 printf "Is the correct certificate and key directory - $script_dir/ ?(y or n): "
 read paths
 printf "Is this a cloudfront certificate? if so enter the path else enter 'n': "
 read cloudfrontpath
 
-if [ "$cloudfrontpath" = "n" ] 
+if [ "$newchain_dir" = "y" ]
 then
-	if [ "$paths" = "y" ]
+	if [ "$cloudfrontpath" = "n" ] 
 	then
-	exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt #This assumes chain lives in the same file as this script
+		if [ "$paths" = "y" ]
+		then
+		exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt #This assumes chain lives in the same file as this script
+		else
+		printf "What is the path the certificate is in?: "
+		read newpaths
+		exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt #This assumes chain lives in the same file as this script
+		fi
 	else
-	printf "What is the path the certificate is in?: "
-	read newpaths
-	exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt #This assumes chain lives in the same file as this script
-	fi
+		if [ "$paths" = "y" ]
+		then
+		exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
+		else
+		printf "What is the path the certificate is in?: "
+		read newpaths
+		exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
+		fi
 else
-	if [ "$paths" = "y" ]
-	then
-	exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
-	else
-	printf "What is the path the certificate is in?: "
-	read newpaths
-	exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$chain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
-	fi
+if [ "$cloudfrontpath" = "n" ]
+        then
+                if [ "$paths" = "y" ]
+                then
+                exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$newchain_dir/$chain.crt #This assumes chain lives in the same file as this script
+                else
+                printf "What is the path the certificate is in?: "
+                read newpaths
+                exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$newchain_dir/$chain.crt #This assumes chain lives in the same file as this script
+                fi
+        else
+                if [ "$paths" = "y" ]
+                then
+                exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$script_dir/$certname/$certname.crt --private-key file://$script_dir/$certname/$certname.key --certificate-chain file://$newchain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
+                else
+                printf "What is the path the certificate is in?: "
+                read newpaths
+                exec aws iam --profile $awsprofile upload-server-certificate --server-certificate-name $certname --certificate-body file://$newpaths/$certname/$certname.crt --private-key file://$newpaths/$certname/$certname.key --certificate-chain file://$newchain_dir/$chain.crt --path $cloudfrontpath #This assumes chain lives in the same file as this script
+                fi
+
 fi
